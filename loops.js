4b64c5a13cf508d62c5235063e4d512e8f15171d
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-07-16 17:46:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,6 +22,6 @@ function maybeTrue() {
 function doWhileLoop(array) {
   do {
     array.pop()
-  }  while (n < 15 && maybeTrue()); 
+  }  while (array.length > 0 && maybeTrue()); 
   return array
 }
